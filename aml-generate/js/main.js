9224b4351432d58b3b6a4dd5b2d2cef27f3196a2
MinhMan.Tran <minhman.tran@ogilvy.com>
2017-05-04 14:39:26 +0700

    del script

--- aml-generate/js/main.js
+++ aml-generate/js/main.js
@@ -15,4 +15,34 @@ $(function(){
             source.append(result);
         });
     });
+
+    source.on('click', '.add-content', function(){
+        var that = $(this),
+            papa = that.parent();
+
+        $('#modalContent').modal('show');
+    });
+
+    $('select[name="selectContentType"]').on('change', function(){
+        if($(this).val() === 'expansion'){
+            $('#modalContent .show-expansion').show();
+            $('#modalContent .show-no-expansion').hide();
+        } else {
+            $('#modalContent .show-expansion').hide();
+            $('#modalContent .show-no-expansion').show();
+        }
+    });
+
+    $('#offerAndJoin').summernote({
+        minHeight: 300
+    });
+
+    $('#hotelHighlight').summernote({
+        minHeight: 300
+    });
+
+    $('#termAndConditions').summernote({
+        minHeight: 300
+    });
+
 });
