43ee12c2b266b76fe8d2821d2c9b8d00178a54d3
MinhMan.Tran <minhman.tran@ogilvy.com>
2017-05-04 10:35:47 +0700

    del script

--- aml-generate/js/main.js
+++ aml-generate/js/main.js
@@ -1,3 +1,18 @@
 /**
  * Created by MinhMan.Tran on 5/3/2017.
  */
+
+$(function(){
+    var source = $('#source');
+    $('#one-column').on('click', function(){
+        $.get('templates/moduleContentOne.html', function(result){
+            source.append(result);
+        });
+    });
+
+    $('#two-columns').on('click', function(){
+        $.get('templates/moduleContentTwo.html', function(result){
+            source.append(result);
+        });
+    });
+});
