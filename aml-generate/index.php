5cc7c54c753e09766ffaccd450005fdd71f3a4a8
jimmyman2010 <tranduyminhman@gmail.com>
2017-05-18 23:42:50 +0700

    change to php

--- aml-generate/index.php
+++ aml-generate/index.php
@@ -107,6 +107,7 @@
                                             </div>
                                         </div>
                                     </div>
+                                    <hr>
                                     <div class="row">
                                         <div class="col-md-4">
                                             <div class="form-group">
@@ -127,7 +128,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <hr>
+
                                     <div class="row">
                                         <div class="col-md-4">
                                             <div class="form-group">
@@ -148,6 +149,7 @@
                                             </div>
                                         </div>
                                     </div>
+                                    <hr>
                                     <div class="row">
                                         <div class="col-md-6">
                                             <div class="form-group">
