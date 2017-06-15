3a24bf43167a04484a734b018a8f479caccf3eea
MinhMan.Tran <minhman.tran@ogilvy.com>
2017-06-15 10:16:49 +0700

    del script

--- aml-generate/templates/pageKV.php
+++
@@ -1,9 +0,0 @@
-<div class="col-xs-12">
-    <?php if($contentPage->kvMobile) { ?>
-    <figure class="hidden-xs hidden-sm"><img src="<?= process_image($contentPage->kvDesktop, $keyPage) ?>" alt="<?= strip_tags($contentPage->kvAlt) ?>" title="<?= strip_tags($contentPage->kvAlt) ?>" class="img-responsive" /></figure>
-    <figure class="visible-xs visible-sm"><img src="<?= process_image($contentPage->kvMobile, $keyPage) ?>" alt="<?= strip_tags($contentPage->kvAlt) ?>" title="<?= strip_tags($contentPage->kvAlt) ?>" class="img-responsive" /></figure>
-    <?php } else { ?>
-    <figure><img src="<?= process_image($contentPage->kvDesktop, $keyPage) ?>" alt="<?= strip_tags($contentPage->kvAlt) ?>" title="<?= strip_tags($contentPage->kvAlt) ?>" class="img-responsive" /></figure>
-    <?php } ?>
-    <h3 class="text-center"><?= process_icon($contentPage->intro) ?></h3>
-</div>
